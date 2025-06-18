de1559814ea81c70ba486f77f0ffea9356bd5d9a
calin9p <calin9p@gmail.com>
2025-06-18 18:03:13 +0300

    Changed Bundle Identifier from co.median.android.wqwnby to co.median.ios.wqwnby

--- lib/presentation/pages/main_screen.dart
+++ lib/presentation/pages/main_screen.dart
@@ -38,10 +38,10 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
 
   final List<String> _bannerImages = [
 
-    'assets/banners/banner1.jpg',
+    'assets/banners/banner4.jpg',
     'assets/banners/banner2.jpg',
     'assets/banners/banner3.jpg',
-    'assets/banners/banner4.jpg',
+    'assets/banners/banner1.jpg',
     'assets/banners/banner5.jpg',
 
   ];
